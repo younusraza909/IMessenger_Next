0b1a7784162a632f9bb18cca3d76615a7f02b6c1
younusraza909 <younusraza909@gmail.com>
2023-02-20 12:23:54 +0500

    Mkaing A create username mutation

--- backend/src/graphql/resolvers/user.ts
+++ backend/src/graphql/resolvers/user.ts
@@ -1,18 +1,54 @@
-import { GraphQlContext } from "../../util/types";
+import { createUsernameResponse, GraphQlContext } from "../../util/types";
 
 const resolvers = {
   Query: {
     searchUsers: () => {},
   },
   Mutation: {
-    createUsername: (
+    createUsername: async (
       _: any,
       args: { username: string },
       context: GraphQlContext
-    ) => {
+    ): Promise<createUsernameResponse> => {
       const { username } = args;
 
       const { session, prisma } = context;
+
+      if (!session?.user) {
+        return {
+          error: "Not authorized",
+        };
+      }
+
+      const { id: userId } = session.user;
+
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: { username },
+        });
+
+        if (existingUser) {
+          return {
+            error: "Username already taken",
+          };
+        }
+
+        await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            username,
+          },
+        });
+
+        return { success: true };
+      } catch (error) {
+        console.log("create user Name error", error);
+        return {
+          error: error?.message,
+        };
+      }
     },
   },
 };
--- backend/src/util/types.ts
+++ backend/src/util/types.ts
@@ -1,5 +1,24 @@
 import { PrismaClient } from "@prisma/client";
-import { Session } from "next-auth";
+import { ISODateString } from "next-auth";
+
+/**
+ * Users
+ */
+
+export interface Session {
+  user: User;
+  expires: ISODateString;
+}
+
+export interface User {
+  id: string;
+  image: string;
+  username: string;
+}
+export interface createUsernameResponse {
+  success?: boolean;
+  error?: string;
+}
 
 export interface GraphQlContext {
   session: Session | null;
