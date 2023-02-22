384cca52f73658c65f20e8eebd0b4e5b562455e5
younusraza909 <younusraza909@gmail.com>
2023-02-22 11:12:18 +0500

    Prisma Schema and create conversation resolver

--- backend/src/graphql/resolvers/conversation.ts
+++ backend/src/graphql/resolvers/conversation.ts
@@ -1,8 +1,64 @@
+import { GraphQlContext } from "../../util/types";
+import { ApolloError } from "apollo-server-core";
+
 const resolvers = {
   Query: {},
   Mutation: {
-    createConversation: async () => {
-      console.log("Create Conversation");
+    createConversation: async (
+      _,
+      args: { participantIds: [String] },
+      context: GraphQlContext
+    ) => {
+      const { session, prisma } = context;
+      const { participantIds } = args;
+
+      if (!session?.user) {
+        throw new ApolloError("Not authorized");
+      }
+
+      const {
+        user: { id: userId },
+      } = session;
+
+      try {
+        const conversation = await prisma.conversation.create({
+          data: {
+            participants: {
+              createMany: {
+                data: participantIds.map((id) => ({
+                  userId: String(id),
+                  hasSeenLatestMessage: id === userId,
+                })),
+              },
+            },
+          },
+          include: {
+            participants: {
+              include: {
+                user: {
+                  select: {
+                    id: true,
+                    username: true,
+                  },
+                },
+              },
+            },
+            latestMessage: {
+              include: {
+                sender: {
+                  select: {
+                    id: true,
+                    username: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+      } catch (error) {
+        console.log("Create Conversation Error", error);
+        throw new ApolloError("Error creating conversation");
+      }
     },
   },
 };
